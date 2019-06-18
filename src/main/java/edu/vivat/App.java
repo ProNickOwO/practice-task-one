cee17237e9a7c546f4cbfbdeff14401f949a71da
ProNickOwO <nichita.lozovan@gmail.com>
2019-06-18 16:45:24 +0300

    Final build
    Signed-off-by: ProNickOwO <nichita.lozovan@gmail.com>

--- src/main/java/edu/vivat/App.java
+++ src/main/java/edu/vivat/App.java
@@ -1,13 +1,26 @@
 package edu.vivat;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Enter a string/number to check if it is a palindrome");
+        String s = in();
+        if (palindrom(s)) System.out.println("Is palindrome");
+        else System.out.println("Is not palindrome");
+    }
+
+    private static String in() {
+        Scanner s = new Scanner(System.in);
+        return s.nextLine();
+    }
+
+    public static boolean palindrom(String s) {
+        String reverse = "";
+        int length = s.length();
+        for (int i = length - 1; i >= 0; i--) {
+            reverse = reverse + s.charAt(i);
+        }
+        return s.equals(reverse);
     }
 }
--- src/test/java/edu/vivat/AppTest.java
+++ src/test/java/edu/vivat/AppTest.java
@@ -1,20 +1,39 @@
 package edu.vivat;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldRecognizeEmptyString() {
+        assertEquals(true, App.palindrom(""));
     }
+
+    @Test
+    public void shouldRecognizeOneCharacterPalindrome() {
+        assertEquals(true, App.palindrom("a"));
+    }
+
+    @Test
+    public void shouldRecognizeTwoCharacterPalindrome() {
+        assertEquals(true, App.palindrom("aa"));
+    }
+
+    @Test
+    public void shouldRecognizeTwoCharacterNonPalindrome() {
+        assertEquals(false, App.palindrom("ab"));
+    }
+
+    @Test
+    public void shouldRecognizePalindrome() {
+        assertEquals(true, App.palindrom("amanaplanacanalpanama"));
+    }
+
+    @Test
+    public void shouldRecognizeNonPalindrome() {
+        assertEquals(false, App.palindrom("noPalindrome"));
+    }
+
 }
